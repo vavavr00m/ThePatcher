42ae552919769cfebc7762bad3df9d771a538d64
vavavr00m <vavavr00m@users.noreply.github.com>
2023-11-02 16:49:03 +0800

    Update ACC.ps1

--- ACC.ps1
+++ ACC.ps1
@@ -8,24 +8,24 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     }
 }
 
-# Add hosts to prevent warning of unlicensed or non-genuine usage
+# Add hosts to prevent warning of unlicensed or non-genuine usage - https://discord.com/channels/808943947660394517/971079960255164477/1155400046532100208
 
 Add-Content -Path $env:windir\System32\drivers\etc\hosts -Value "`n0.0.0.0`tpojvrj7ho5.adobe.io" -Force
 Add-Content -Path $env:windir\System32\drivers\etc\hosts -Value "`n0.0.0.0`tic.adobe.io" -Force
 
-# Stop Creative Cloud
+# Stop Creative Cloud - https://discord.com/channels/808943947660394517/971079960255164477/1166781698684498053
 
 Stop-Process -Name "Adobe Desktop Service" -force
 
-# Uninstall AGS - Adobe Genuine Service
+# Uninstall AGS - Adobe Genuine Service - https://discord.com/channels/808943947660394517/971079960255164477/1166781698684498053
 
 [System.Diagnostics.Process]::Start("C:\Program Files (x86)\Common Files\Adobe\AdobeGCClient\AdobeCleanUpUtility.exe")
 
-# Back up AppsPanelBL.dll
+# Back up AppsPanelBL.dll - https://discord.com/channels/808943947660394517/971079960255164477/1153941241927565384
 
 cp "C:\Program Files (x86)\Common Files\Adobe\Adobe Desktop Common\AppsPanel\AppsPanelBL.dll" "C:\Program Files (x86)\Common Files\Adobe\Adobe Desktop Common\AppsPanel\AppsPanelBL.dll.bak"
 
-# Patch AppsPanelBL.dll
+# Patch AppsPanelBL.dll - https://discord.com/channels/808943947660394517/971079960255164477/1153941072637079593
 
 $bytes  = [System.IO.File]::ReadAllBytes("C:\Program Files (x86)\Common Files\Adobe\Adobe Desktop Common\AppsPanel\AppsPanelBL.dll")
 $bytes[1116554] = 0xfe
